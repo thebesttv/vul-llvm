3986d3e867d3ff3ef3d63b0db98f5a0985843758
thebesttv <taojuntian@outlook.com>
2024-07-14 17:35:05 +0800

    Remove unused binaries when building AppImage

--- create-appimage.sh
+++ create-appimage.sh
@@ -9,6 +9,15 @@ ninja
 rm -rf AppDir
 mkdir AppDir
 DESTDIR=AppDir ninja install || true
+
+# remove unused binaries
+cd AppDir/usr/bin/ && rm $(ls | \
+    grep -v thebesttv | \
+    grep -v '^clang$' | \
+    grep -v '^clang-17$' | \
+    grep -v '^clang++$' \
+) && cd ../../..
+
 # Do not strip libraries when running local (ie., when GA is not set).
 # In ArchLinux, trying to strip libraries will cause error
 if [ -z "$GITHUB_ACTIONS" ]; then
