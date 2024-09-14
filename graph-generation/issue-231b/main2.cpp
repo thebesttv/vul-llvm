e0d2fbb3e90b93e67423a89d17a9098653770e2a
thebesttv <taojuntian@outlook.com>
2024-09-14 15:28:48 +0800

    Add initial testcase

---
+++ graph-generation/issue-231b/main2.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+template <class T>
+class TestClass1 {
+public:
+    TestClass1(T *data) : data_(data)
+    {
+        ptr = &data->ptr; // sink
+    };
+    
+protected:
+    T *data_;
+    int **ptr;
+};
+
+template <class T>
+class TestClass2 : public TestClass1<T> {
+public:
+    explicit TestClass2(const T *table) : 
+		TestClass1<T>(const_cast<T *>(table)){};
+};
+
+struct TestStru {
+    int num;
+    int *ptr;
+};
+
+TestStru *GetData();
+void UsePtr2(const TestClass2<TestStru> &table);
+
+void UsePtr(const TestStru &table)
+{
+    TestClass2<TestStru> table2(&table);
+    UsePtr2(table2);
+}
+
+// 1441151880815120306
+void test()
+{
+    TestStru *table = GetData(); // source
+    UsePtr(*table);
+}
